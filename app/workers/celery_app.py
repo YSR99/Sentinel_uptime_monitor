78cea56b29db57babbdaad096ba1889c4b1e4f93
ysr99 <yuvi8313@gmail.com>
2026-06-09 13:25:04 +0530

    Redis congigured for deployment

--- app/workers/celery_app.py
+++ app/workers/celery_app.py
@@ -1,10 +1,15 @@
+import os
 from celery import Celery
 
+REDIS_URL = os.getenv(
+    "REDIS_URL",
+    "redis://localhost:6379/0"
+)
 
 celery = Celery(
     "sentinel",
-    broker="redis://localhost:6379/0",
-    backend="redis://localhost:6379/0"
+    broker=REDIS_URL,
+    backend=REDIS_URL
 )
 
 
